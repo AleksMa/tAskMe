8e3e0af98f20c0dbd66675579828fc511ebfa705
aleksma <aleksma99@yandex.ru>
2019-04-02 19:52:03 +0300

    [ADD] html

--- questions/urls.py
+++ questions/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.question_list, name='index'),
     path('base', views.base, name='base'),
     path('question', views.question, name='question'),
+    path('ask', views.ask, name='ask'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('settings', views.settings, name='settings'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -8,8 +8,19 @@ def base(request):
     return render(request, 'questions/base.html', {})
 
 def question(request):
-    return render(request, 'questions/question.html', {})
+    return render(request, 'questions/quest.html', {})
 
+def ask(request):
+    return render(request, 'questions/ask.html', {})
+
+def login(request):
+    return render(request, 'questions/login.html', {})
+
+def signup(request):
+    return render(request, 'questions/signup.html', {})
+
+def settings(request):
+    return render(request, 'questions/settings.html', {})
 
 # class AboutView(TemplateView):
 #     template_name = "questions/base.html"
