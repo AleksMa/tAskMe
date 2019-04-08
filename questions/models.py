3fe36e1b23c1f91a930cece4b4e8e93ed379e134
aleksma <aleksma99@yandex.ru>
2019-04-08 07:21:20 +0300

    [ADD] simple models

--- questions/models.py
+++ questions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Question(models.Model):
+class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -16,3 +16,35 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+class User(models.Model):
+    email = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    photo = models.ImageField
+    created_date = models.DateTimeField(default=timezone.now)
+    rate = models.IntegerField(default=0)
+
+class Tag(models.Model):
+    text = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.text
+
+
+
+class Question(models.Model):
+    title = models.TextField
+    content = models.TextField
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag)
+    rate = models.IntegerField
+
+class Answer(models.Model):
+    content = models.TextField
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    isRight = models.BooleanField
+    rate = models.IntegerField
--- questions/views.py
+++ questions/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import TemplateView
+from .models import Question
+
+
+
+questions = []
+
+
+
+for i in range(1,10):
+  questions.append({
+    'id': 3 * (i - 1) + 1,
+   'title': "Linear algebra objective type question",
+   'content': "Let M be an n×n hermition matrix of rank k if λ≠0 be an eigen value of M with corresponding unit column vector as eigen vector u i.e. Mu=λu. Then we must have...",
+    'tags': "linear-algebra matrices",
+    'author': "MiSa"
+  })
+  questions.append({
+      'id': 3 * (i - 1) + 2,
+      'title': "Number of five digit numbers that can be formed with this condition",
+      'content': "How many five digit numbers can be made having the digits 1,2,3 each of which can be used at most thrice in a number?",
+      'tags': "linear-algebra matrices",
+      'author': "Danil"
+  })
+  questions.append({
+      'id': 3 * (i - 1) + 3,
+      'title': "Difference between Riemann sums and Riemann integrals",
+      'content': "Let M be an n×n hermition matrix of rank k if λ≠0 be an eigen value of M with corresponding unit column vector as eigen vector u i.e. Mu=λu. Then we must have...",
+      'tags': "linear-algebra matrices",
+      'author': "Anton"
+  })
+
+
 
 def question_list(request):
-    return render(request, 'questions/index.html', {})
+    #questions = Question.objects.all()
+    return render(request, 'questions/index.html', {'questions': questions})
+
+
+
+
+    #return render(request, 'questions/index.html', {})
 
 def base(request):
     return render(request, 'questions/base.html', {})
@@ -26,6 +65,3 @@ def settings(request):
 #     template_name = "questions/base.html"
 
 
-QUESTIONS = {
-
-}
